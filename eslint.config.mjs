684b4563982285991ac7faaf01910be33d49bc91
Jimmy Callin <jimmy.callin@gmail.com>
2025-03-03 16:20:02 +0100

    chore(deps): Upgrade all dependencies (#48)

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+// @ts-check
+
+export default tseslint.config(
+  {
+    ignores: ["dist"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
